ddbd39bb5377cc737744f09c4068d3f82bff2e0b
Jasper Alblas <just_jasper@hotmail.com>
2022-10-31 17:13:01 +0100

    Few improvements

--- src/App.js
+++ src/App.js
@@ -56,10 +56,10 @@ const Person = ({ person }) => {
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+    { name: 'Arto Hellas', number: '040-123456', id: 0 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 1 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 2 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 3 }
   ]);
   const [newFilter, setFilter] = useState("");
   const [newName, setNewName] = useState("");
@@ -70,11 +70,12 @@ const App = () => {
     if (persons.some((person) => person.name === newName)) {
       alert(`${newName} is already added to the phonebook`);
     } else {
-      const newUser = { name: newName, number: newNumber };
+      const newUser = { name: newName, number: newNumber, id: persons.length };
       setPersons(persons.concat(newUser));
     }
 
     setNewName("");
+    setNewNumber("");
   };
 
   const handleNameChange = (event) => {
@@ -89,7 +90,7 @@ const App = () => {
     setFilter(event.target.value);
   }
 
-  const visiblePersons = newFilter.length !== 0 ? persons.filter((person) => person.name === newFilter) : persons;
+  const visiblePersons = newFilter.length !== 0 ? persons.filter((person) => person.name.toLowerCase().includes(newFilter.toLowerCase())) : persons;
 
   return (
     <div>
