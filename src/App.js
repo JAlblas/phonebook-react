ad8151a2911be86c67523427eb75b821289f517a
Jasper Alblas <just_jasper@hotmail.com>
2022-11-01 05:55:15 +0100

    Now fetched persons with axios

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 import { useState, useEffect } from "react";
+import axios from 'axios'
 
 import Filter from './modules/Filter'
 import Form from './modules/Form'
 import Phonebook from "./modules/Phonebook";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 0 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 1 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 2 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 3 }
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newFilter, setFilter] = useState("");
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
+
+
+
 
   const addUser = (event) => {
     event.preventDefault();
