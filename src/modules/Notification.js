9c3eabe523fccb7c112b85e15be59fa3637e3c68
Jasper Alblas <just_jasper@hotmail.com>
2022-11-05 21:28:43 +0100

    Message style now set with inline styles

--- src/modules/Notification.js
+++ src/modules/Notification.js
@@ -1,13 +1,42 @@
 const Notification = ({ message, type }) => {
+    const errorStyle = {
+        color: 'red',
+        background: 'lightgrey',
+        fontSize: 20,
+        borderStyle: 'solid',
+        borderRadius: 5,
+        padding: 10,
+        marginBottom: 10
+    }
+
+    const succesStyle = {
+        color: 'green',
+        background: 'lightgrey',
+        fontSize: 20,
+        borderStyle: 'solid',
+        borderRadius: 5,
+        padding: 10,
+        marginBottom: 10
+    }
+
     if (message === null) {
         return null
     }
 
-    return (
-        <div className={type}>
-            {message}
-        </div>
-    )
+    if (type === 'success') {
+        return (
+            <div style={succesStyle}>
+                {message}
+            </div>
+        )
+    } else if (type === 'error') {
+        return (
+            <div style={errorStyle}>
+                {message}
+            </div>
+        )
+    }
 }
 
 export default Notification
+
